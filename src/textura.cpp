718f675761f6babf18ad1cf1aaaa4c3541c0890d
explodingnuggets <matheus.silva777@etec.sp.gov.br>
2017-03-28 19:47:19 -0300

    Adicionado arquivos base e classes das cartas e texturas.

---
+++ src/textura.cpp
@@ -0,0 +1,47 @@
+#include "textura.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+SDL_Texture* Textura::loadTexture(){
+	// Textura final
+	SDL_Texture* newTexture = NULL;
+	
+	// Carrega imagem a partir de um caminho
+	SDL_Surface* loadedSurface = IMG_Load(this->pPath.c_str());
+	if(loadedSurface == NULL){
+		cout << "Unable to load image " << this->pPath.c_str() << ". SDL_Image Error: " << IMG_GetError() << endl;
+	} else {
+		// Cria textura dos pixels da superficie
+		newTexture = SDL_CreateTextureFromSurface(this->pRenderer, loadedSurface);
+		if( newTexture == NULL) {
+			cout << "Unable to create texture from " << this->pPath.c_str() << ". SDL Error: " << SDL_GetError() << endl;
+		}
+		
+		// Deleta a superficie
+		SDL_FreeSurface(loadedSurface);
+	}
+	
+	return newTexture;
+}
+
+Textura::Textura() {}
+
+Textura::Textura(string path, SDL_Renderer* renderer, int x, int y, int w, int h){
+	this->pRenderer = renderer;
+	this->pPath = path;
+	this->pTexture = this->loadTexture();
+	this->recFormat.x = x;
+	this->recFormat.y = y;
+	this->recFormat.w = w;
+	this->recFormat.h = h;
+}
+
+Textura::~Textura(){
+	SDL_DestroyTexture(this->pTexture);
+}
+
+void Textura::render(){
+	SDL_RenderCopy(this->pRenderer, this->pTexture, NULL, &this->recFormat);
+}
---
+++ src/textura.h
@@ -0,0 +1,23 @@
+#ifndef TEXTURA_H
+#define TEXTURA_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <string>
+
+class Textura {
+	SDL_Rect recFormat;
+	SDL_Texture* pTexture;
+	SDL_Renderer* pRenderer;
+	std::string pPath;
+	
+	SDL_Texture* loadTexture();
+	public:
+	Textura();
+	Textura(std::string, SDL_Renderer*, int, int, int, int);
+	~Textura();
+	
+	void render();
+};
+
+#endif
