e2743a0b10fee5ed38b109254775ba0a30589f70
Gabriel <g4briel.4lves@gmail.com>
2017-03-31 13:56:27 -0300

    Implementadas as Pilhas Inteligentes
    falta implementar a verificação por naipe no push(), porque faltou documentação sobre os naipes em getSuit().

--- Source/include/pilha.h
+++ Source/include/pilha.h
@@ -1,41 +1,18 @@
 #ifndef PILHA_H
 #define PILHA_H
 
-#include <iostream>
-using namespace std;
-
 template <class T>
 class Stack{
 public:
-    Stack(int s){
-        size = (s > 0 ? s : 10);
-        top = -1;
-        stackPtr = new T[size];
-    }
+    Stack(int s);
     ~Stack(){delete []stackPtr;};
 
-    bool push(const T& pushValue){
-      if (!isFull()){
-          stackPtr[++top] = pushValue;
-          return true;
-      }
-      return false;
-    };
+    bool push(const T&);
+    bool pop(T&);
 
-    bool pop(T& popValue){
-      if (!isEmpty()){
-        popValue = stackPtr[top--];
-        return true;
-      }
-      return false;
-    };
-
-    bool isEmpty() const{
-      return top == -1;
-    }
-    bool isFull() const{
-      return top == size - 1;
-    };
+    T peek(){return this->stackPtr[top];};
+    bool isEmpty() const{return this->top == -1;};
+    bool isFull() const{return this->top == size - 1;};
 
 private:
     int size;
@@ -43,4 +20,28 @@ private:
     T *stackPtr;
 };
 
+template <class T>
+Stack<T>::Stack(int s){
+    this->size = (s > 0 ? s : 10);
+    this->top = -1;
+    this->stackPtr = new T[this->size];
+}
+
+template <class T>
+bool Stack<T>::push(const T& pushValue){
+    if (!this->isFull()){
+        this->stackPtr[++top] = pushValue;
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+bool Stack<T>::pop(T& popValue){
+    if (!this->isEmpty()){
+        popValue = this->stackPtr[top--];
+        return true;
+    }
+    return false;
+}
 #endif
--- Source/include/pilha_inteligente.h
+++ Source/include/pilha_inteligente.h
@@ -3,17 +3,43 @@
 
 #include "pilha.h"
 #include "carta.h"
-using namespace std;
 
 class PilhaIntermediaria : public Pilha{
 public:
-  push(){}
+  bool push(const Carta&);
+  int peekValue(){return this->stackPtr[top].getValue();};
+  char peekSuit(){return this->stackPtr[top].getSuit();};
 };
+
+bool PilhaIntermediaria::push(const Carta& pushValue){
+  if(!this->isFull()){
+    if(pushValue.getValue() == this->peekValue()-1){
+      //if(pushValue.getSuit != this->peekSuit()){
+        this->stackPtr[++top] = pushValue;
+        return true;
+    //}
+    }
+  }
+  return false;
+}
 
 class PilhaDefinitiva : public Pilha{
 public:
-  push(){}
+  push(const Carta&);
+  int peekValue(){return this->stackPtr[top].getValue();};
+  char peekSuit(){return this->stackPtr[top].getSuit();};
 };
 
+bool PilhaIntermediaria::push(const Carta& pushValue){
+  if(!this->isFull()){
+    if(pushValue.getValue() == this->peekValue()+1){
+      //if(pushValue.getSuit == this->peekSuit()){
+        this->stackPtr[++top] = pushValue;
+        return true;
+    //}
+    }
+  }
+  return false;
+}
 
 #endif
