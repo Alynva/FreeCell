3a800660a77ef9970aa36a32e93d2d751b7812a2
Alisson Nunes <alynva@gmail.com>
2017-04-17 11:01:33 -0300

    Separado o Node da Stack

--- Source/include/node.h
+++ Source/include/node.h
@@ -2,101 +2,43 @@
 #define NODE_H
 
 template<class T>
-class Node{
-private:
-	T element;
-	Node* next;
-
-public:
-  Node(T);
-  ~Node();
-	void setNext(Node*);
-  Node* getNext() const;
-  T getElement() const;
+class Node {
+	private:
+		T element;
+		Node* next;
+	
+	public:
+		Node(T);
+		~Node();
+		void setNext(Node*);
+		Node* getNext() const;
+		T getElement() const;
 };
 
 template<class T>
-Node<T>::~Node(){
+Node<T>::~Node() {
 	delete this->next;
 }
 
 template<class T>
-Node<T>::Node(T elementVal){
+Node<T>::Node(T elementVal) {
 	this->element = elementVal;
 	this->setNext(NULL);
-}
+}s
 
 template<class T>
-void Node<T>::setNext(Node* nextVal){
+void Node<T>::setNext(Node* nextVal) {
 	this->next = nextVal;
 }
 
 template<class T>
-Node* Node<T>::getNext() const{
-  return this->next;
+Node* Node<T>::getNext() const {
+	return this->next;
 }
 
 template<class T>
-T Node<T>::getElement() const{
-  return  this->element;
-}
-
-class Stack{
-private:
-  Node* stack_ptr;
-  int size;
-
-public:
-  Stack();
-	~Stack();
-  void push(const T&);
-  bool pop(Node);
-  bool isEmpty() const;
-  int getSize() const;
-	Node peek() const;
-};
-
-Stack::Stack(){
-  this->stack_ptr = NULL;
-  this->size = 0;
-}
-
-Stack::~Stack(){
-	delete this->stack_ptr;
-}
-
-template<class T>
-void Stack::push(const T& pushValue){
-  Node* aux = new Node<T>(pushValue);
-  aux.setNext(stack_ptr);
-  this->stack_ptr = *aux;
-  this->tamanho++;
-}
-
-bool Stack::pop(Node popValue){
-  if(!this->isEmpty()){
-    Node* aux = this->stack_ptr;
-    popValue = *this->stack_ptr;
-		delete this->stack_ptr;
-		this->stack_ptr = aux->next;
-    return true;
-  }
-  return false;
-}
-
-bool Stack::isEmpty() const{
-  if(!this->stack_ptr){
-    return true;
-  }
-  return false;
-}
-
-int Stack::getSize() const{
-  return this->size;
-}
-
-Node Stack::peek() const{
-	return *this->stack_ptr;
+T Node<T>::getElement() const {
+	return  this->element;
 }
 
 #endif
