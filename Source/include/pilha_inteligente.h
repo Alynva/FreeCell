254ca9828b12db813e389f82092c82af5dd1ec53
Gabriel <g4briel.4lves@gmail.com>
2017-04-28 17:39:56 -0300

    Agora usando template especializado

--- Source/include/pilha_inteligente.h
+++ Source/include/pilha_inteligente.h
@@ -12,7 +12,8 @@
 #define CARD_WIDTH 100 // largura final do fundo da pilha
 #define CARD_HEIGHT 156 // altura final do fundo da pilha
 
-class PilhaInteligente : public Stack {
+template<>
+class PilhaInteligente<Carta> : public Stack {
 	private:
 		SDL_Point coord;
 		Textura backTexture;
@@ -27,11 +28,13 @@ class PilhaInteligente : public Stack {
 		bool isInside(SDL_Point);
 };
 
-PilhaInteligente::PilhaInteligente() {
+template<>
+PilhaInteligente<Carta>::PilhaInteligente() {
 	this->coord = {0, 0};
 }
 
-bool PilhaInteligente::setTexture(SDL_Renderer* renderer) {
+template<>
+bool PilhaInteligente<Carta>::setTexture(SDL_Renderer* renderer) {
 	this->backTexture = Textura("../textures/pilha.png", renderer, this->coord.x, this->coord.y, 69, 100);
 	int w = CARD_WIDTH, h = CARD_HEIGHT;
 	if(!SDL_QueryTexture(this->backTexture.getTexture(), NULL, NULL, &w, &h)){
@@ -40,13 +43,15 @@ bool PilhaInteligente::setTexture(SDL_Renderer* renderer) {
 	return false;
 }
 
-bool PilhaInteligente::setPosition(SDL_Point pos) {
+template<>
+bool PilhaInteligente<Carta>::setPosition(SDL_Point pos) {
 	this->coord = pos;
 
 	return true; // Por hora, nao faz nenhuma verificacao de ser negativo ou ultrapassar a tela
 }
 
-void PilhaInteligente::randomize() {
+template<>
+void PilhaInteligente<Carta>::randomize() {
 	/*
 	// Alynva: aguardando a implementação da sobrecarga do operador []
 	
@@ -65,7 +70,8 @@ void PilhaInteligente::randomize() {
 	*/
 }
 
-void PilhaInteligente::render() {
+template<>
+void PilhaInteligente<Carta>::render() {
 	this->backTexture.render();
 
 	Stack<Carta> p_temp;
@@ -82,7 +88,8 @@ void PilhaInteligente::render() {
 	}
 }
 
-void PilhaInteligente::organize() {
+template<>
+void PilhaInteligente<Carta>::organize() {
 	
 	Stack<Carta> p_temp;
 	Carta c_temp;
@@ -100,7 +107,8 @@ void PilhaInteligente::organize() {
 	}
 }
 
-bool PilhaInteligente::isInside(SDL_Point point) {
+template<>
+bool PilhaInteligente<Carta>::isInside(SDL_Point point) {
 	bool inside = false;
 	
 	Stack<Carta> p_temp;
